24caeff5ac900afad73389f3f5bde2dca02980e7
Antoni Słomka <s33610@pjwstk.edu.pl>
2026-03-17 12:56:32 +0100

    Added abstract Equipment class

---
+++ Models/Equipment.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APBD_TASK2.Models
+{
+    public abstract class Equipment
+    {
+        private static int _nextId = 1;
+
+        public int Id { get; }
+        public string Name { get; set; } = null!;
+        public string Description { get; set; } = null!;
+
+        public DateTime dateAdded { get; set; }
+
+    }
+}
