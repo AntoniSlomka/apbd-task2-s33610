5d9a1cb7c3744cec7ec66eb17f42bec81f62f697
Antoni Słomka <s33610@pjwstk.edu.pl>
2026-03-17 13:37:19 +0100

    Added Laptop and Camera Equipment

---
+++ Models/Camera.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APBD_TASK2.Models
+{
+    internal class Camera : Equipment
+    {
+
+        public int ResolutionMP { get; set; }
+        public int MinimumISO { get; set; }
+        public int MaximumISO { get; set; }
+        public double LensFocalLength { get; set; }
+        public bool recordingCapabilities { get; set; }
+
+        public Camera(string name, string description, int resolutionMP, int minimumISO, int maximumISO, double lensFocalLength, bool recordingCapabilities) 
+            : base(name, description = "Camera")
+        {
+            ResolutionMP = resolutionMP;
+            MinimumISO = minimumISO;
+            MaximumISO = maximumISO;
+            LensFocalLength = lensFocalLength;
+            this.recordingCapabilities = recordingCapabilities;
+        }
+
+    }
+}
---
+++ Models/Laptop.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APBD_TASK2.Models
+{
+    internal class Laptop : Equipment
+    {
+        public int RamGB { get; set; }
+        public int ScreenSizeInch {  get; }
+        
+        public Laptop(string name, string description, int ramGB, int screenSize) : base (name, description = "Laptop") 
+        {
+            this.RamGB = ramGB;
+            this.ScreenSizeInch = screenSize;
+        }
+
+    }
+}
